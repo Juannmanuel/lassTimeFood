1756cbd3b9fc625c3f86382edb544883e0ca8b9e
Juan Manuel <losredondos551@gmail.com>
2023-12-28 17:51:42 -0300

    detail tablet completo

--- src/components/Navbar/SearchBar/SearchBar.jsx
+++ src/components/Navbar/SearchBar/SearchBar.jsx
@@ -2,9 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import style from "./SearchBar.module.css";
 import { useState } from "react";
 import { SearchFood } from "../../../redux/actions";
+import { Link } from "react-router-dom";
 
 function SearchBar() {
   const resultSearch = useSelector((state) => state.search);
+  console.log(resultSearch, "results");
   const dispatch = useDispatch();
   const [isSearching, setIsSearching] = useState(false);
 
@@ -17,13 +19,22 @@ function SearchBar() {
   return (
     <div className={style.searchBar_main}>
       <div className={style.input_searchBar}>
-        <input type="text" placeholder="Search for your favorite food." onChange={handleChange} />
+        <input
+          type="text"
+          placeholder="Search for your favorite food."
+          onChange={handleChange}
+        />
       </div>
       {isSearching && resultSearch && resultSearch.length > 0 ? (
         <div className={style.content_result_search}>
           {resultSearch.map((item, index) => (
             <div key={index} className={style.result_search}>
-              <div className={style.container_image_search}><img src={item.strMealThumb} alt={item.strMeal} /></div>
+              <div className={style.container_image_search}>
+                {" "}
+                <Link to={`/detail/${item.idMeal}`}>
+                  <img src={item.strMealThumb} alt={item.strMeal} />{" "}
+                </Link>
+              </div>
               <div className={style.container_title_search}>{item.strMeal}</div>
             </div>
           ))}
