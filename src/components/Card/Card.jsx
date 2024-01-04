e576b5f1174d00a5dd3209978a8df770ba907c1b
Juan Manuel <losredondos551@gmail.com>
2024-01-04 15:03:31 -0300

    favorite

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import style from "./Card.module.css";
 import { IoMdStar } from "react-icons/io";
+import Favorite from "./Favorite/Favorite";
 
 function Card({ strMeal, idMeal, strMealThumb }) {
   return (
     <section className={style.card_main} style={{backgroundImage: `url(${strMealThumb})`}}>
       <div className={style.score_food}><span><IoMdStar />5.0</span>
+      </div>
+      <div className={style.container_favorite}>
+        <Favorite/>
       </div>
       <div className={style.name_food}>
         <Link to={`/detail/${idMeal}`}>
