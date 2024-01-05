06c18469de871c94051ede3bd6056d94753711f2
Juan Manuel <losredondos551@gmail.com>
2024-01-05 16:30:20 -0300

    responsive geader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,45 +15,41 @@ function Header() {
 
       <div className={style.welcome_header}>
         <div className={style.title_header}>
-          <img src={logo} alt="lassTimeFood" />
+          <img src={logo} alt="LastTimeFood" />
         </div>
         <div className={style.description_header}>
           <div className={style.description_header_title}>
             <FaCheck style={{ color: "green" }} />
-            <span>Explora una Colección de 500+ Recetas</span>
+            <span>Explore a Collection of 500+ Recipes</span>
           </div>
           <div className={style.description_header_title}>
             <FaCheck style={{ color: "green" }} />
-            <span>Descubre Más de 600 Ingredientes Únicos</span>
+            <span>Discover More than 600 Unique Ingredients</span>
           </div>{" "}
           <div className={style.description_header_title}>
             <FaUtensils />
-            <span>Déjate Seducir por una Selección de 400+ Platos</span>
+            <span>Indulge in a Selection of 400+ Dishes</span>
           </div>{" "}
           <div className={style.description_header_title}>
             <FiUsers />
-            <span>Únete a la Comunidad Culinary World</span>
+            <span>Join the Culinary World Community</span>
           </div>
           <div className={style.description_header_title}>
             <FiBook style={{ width: "30px" }} />
             <span>
-              Comparte Tu Experiencia y Conéctate con Otros Aficionados
+              Share Your Experience and Connect with Other Enthusiasts
             </span>
           </div>
         </div>
         <div className={style.login_header}>
           <section>
             <p>
-              Inicia sesión o regístrate para disfrutar de todas las funciones
-              de nuestra comunidad culinaria. Comparte tus creaciones, conecta
-              con otros entusiastas de la cocina, deja reseñas sobre los platos
-              de los demás y marca tus recetas favoritas. Únete a nosotros y
-              lleva tu experiencia culinaria al siguiente nivel.
+              Log in or sign up to enjoy all the features of our culinary community. Share your creations, connect with other kitchen enthusiasts, leave reviews on others' dishes, and bookmark your favorite recipes. Join us and take your culinary experience to the next level.
             </p>
           </section>
           <div className={style.btn_login}>
             <button>Login</button>
-            <button>Signin</button>
+            <button>Sign up</button>
           </div>
         </div>
       </div>
