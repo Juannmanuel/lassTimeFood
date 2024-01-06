2b1647f492fa2d5cb7ce9c00b97f2b02174ad827
Juan Manuel <losredondos551@gmail.com>
2024-01-05 22:09:47 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,26 @@
-import style from "./About.module.css"
+import style from "./About.module.css";
 
-function About(){
-
-    return (
-        <div className={style.about_main}>
-            <h2>About</h2>
-        </div>
-    )
+function About() {
+  return (
+    <div className={style.about_main}>
+      <div className={style.container_image}>
+        <img
+          src="https://juan-manuel.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FimageProfile.edcf572f.jpeg&w=256&q=95"
+          alt=""
+        />
+      </div>
+      <section className={style.container_slogan}>
+        <span>
+          Hi, my name is Juan Manuel. I am a full-stack developer. My job is to{" "}
+          <span className={style.underline}>
+            turn ideas into user experiences
+          </span>
+          .
+        </span>
+      </section>
+      <footer className={style.container_redes}>redes</footer>
+    </div>
+  );
 }
 
-
-export default About
+export default About;
