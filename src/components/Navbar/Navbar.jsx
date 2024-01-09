747d1b1f447b04c3a39a6de2ee0fb4c88be97d79
Juan Manuel <losredondos551@gmail.com>
2024-01-09 15:22:12 -0300

    logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,21 +2,22 @@ import { useSelector } from "react-redux";
 import style from "./Navbar.module.css";
 import MenuMobile from "./MenuMobile/MenuMobile";
 import SearchBar from "./SearchBar/SearchBar";
+import logoNav from "../../assets/images/logoNav.jpeg"
 
 function Navbar() {
     const categories = useSelector((state) => state.categories)
   return (
     <nav className={style.nav_main}>
-      <div className={style.container_logo}>logo</div>
+      <div className={style.container_logo}><img src={logoNav} alt="logo" /></div>
       <div className={style.container_links}>
         <div className={style.dropdown}>
-          <button id="title_nav" className={style.dropbtn}>CATEGORIES</button>
+          <button id="title_nav" className={style.dropbtn}>Categories</button>
           <div className={style.dropdown_content}>
           {categories.map((item, index) => <a key={index} href={`/category/${item}`}>{item}</a>)}
           </div>
         </div>
-        <button id="title_nav"><a href="/">HOME</a></button>
-        <button id="title_nav"><a href="/about">ABOUT</a></button>
+        <button id="title_nav"><a href="/">Home</a></button>
+        <button id="title_nav"><a href="/about">About</a></button>
       </div>
       <div className={style.container_search}><SearchBar/></div>
       <div className={style.menu_mobile}><MenuMobile/></div>
