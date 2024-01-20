34364e2b2349260533a12a8b9794230a8c4766a5
Juan Manuel <losredondos551@gmail.com>
2024-01-20 01:51:51 -0300

    footer en ingles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,25 @@
-import style from "./Footer.module.css"
-import { Link } from "react-router-dom"
+import style from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
-function Footer (){
-
-    return (
-        <footer className={style.footer}>
-        <small className={`${style.smallText} block text-xs`}>
-         <Link to={"https://juan-manuel.vercel.app"} target="_blank" style={{textDecoration: "none", color:"#e2e2e3"}}>&copy; 2023 Juan Manuel.</Link>
-        </small>
-        <p className={`text-xs ${style.infoText}`}>
-          <span className={style.boldText}>Acerca de este sitio web:</span> construido con
-          React Vite (App Router y Server Actions), JavaScript, CSS module, alojado en Vercel.
-        </p>
-      </footer>
-    )
+function Footer() {
+  return (
+    <footer className={style.footer}>
+      <small className={`${style.smallText} block text-xs`}>
+        <Link
+          to={"https://juan-manuel.vercel.app"}
+          target="_blank"
+          style={{ textDecoration: "none", color: "#e2e2e3" }}
+        >
+          &copy; 2023 Juan Manuel.
+        </Link>
+      </small>
+      <p className={`text-xs ${style.infoText}`}>
+        <span className={style.boldText}>About this website:</span> built with
+        React Vite (App Router and Server Actions), JavaScript, CSS modules,
+        hosted on Vercel.
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
